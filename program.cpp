c610ae0a2fc7766fb7ab598e7239a2ccff0c9214
JATINISANMATER2563 <dsugar301@gmail.com>
2017-11-16 10:02:20 +0100

    ispis izbornika

--- program.cpp
+++ program.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 int main()
 {
+    cout <<"1.-racunsko pojacalo u invertirajucoj zatvorenoj petlji"<<endl;
+    cout <<"2.-Integrator"<<endl;
+    cout <<"3.-Derivator"<<endl;
+    cout <<"4.-Tezinsko zbrajalo"<<endl;
+    cout <<"5.-Racunsko pojacalo u neinvertirajucoj zatvorenoj petlji"<<endl;
+    cout <<"6.-gustoca magnetskog toka(magnetska indukcija)"<<endl;
+    cout <<"7.-Jakost magnetskog polja unutar ravnog vodica"<<endl;
+    cout <<"8.-Stupanj iskoristivosti realnog stroja"<<endl;
+    cout <<"9.-Brzina vrtnje sinkronog stroja"<<endl;
+    cout <<"10.-Klizanje asinkronog motora"<<endl;
+    int odabir;
+    cin <<"odaberite broj 1-10 s odgovarajucom formulom!" <<endl;
+    cin>>odabir;
+    switch(odabir)
+
     return 0;
 }
